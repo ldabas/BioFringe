be5fb61dc20f0b7f724948cb165813eec8089541
Vyron Avramidis <vyronavramidis@gmail.com>
2023-07-26 19:05:13 +0300

    Adding changes from main / Refactoring

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,12 @@ import streamlit as st
 
 @st.cache_data
 def get_data() -> pd.DataFrame:
-    return pd.read_csv('./viable_dataset.csv')
+    return pd.read_csv('./data/viable_dataset.csv')
+
+def append_data(new_data):
+    new_entry = pd.Series(new_data)
+    st.session_state.data = pd.concat([st.session_state.data, new_entry.to_frame().T]).reset_index(drop=True)
+    st.session_state.data.to_csv('./data/viable_dataset.csv', index=False)
 
 def add_values():
     with st.form("add_new_data"):
@@ -14,16 +19,16 @@ def add_values():
         col1, col2, col3 = st.columns(3)
         # TODO: Naming of vars is horrendous. We need to change that to something readable
         with col1:
-            new_date = st.date_input("Fill in **Date**")
-            new_ps_q_day = st.number_input("Fill in **PS_Q_DAY**")
+            new_date = st.text_input("Enter **Date**")
+            new_ps_q_day = st.number_input("Enter **PS_Q_DAY**")
         
         with col2:
-            new_tps_q1_day = st.number_input("Fill in **TPS_Q1_DAY**")
-            new_twas_daf_qin_day = st.number_input("Fill in **TWAS_DAF_QIN_DAY**")
+            new_tps_q1_day = st.number_input("Enter **TPS_Q1_DAY**")
+            new_twas_daf_qin_day = st.number_input("Enter **TWAS_DAF_QIN_DAY**")
         
         with col3:
-            new_dig_s_qout_day = st.number_input("Fill in **DIGESTED_SLUDGE_QOUT_DAY**")
-            new_dig_s_dwtr_ds_after_per_week = st.number_input("Fill in **DIG_SLUDGE_DEWATER_DS_AFTER_DEWATER_3_PER_WEEK**")
+            new_dig_s_qout_day = st.number_input("Enter **DIGESTED_SLUDGE_QOUT_DAY**")
+            new_dig_s_dwtr_ds_after_per_week = st.number_input("Enter **DIG_SLUDGE_DEWATER_DS_AFTER_DEWATER_3_PER_WEEK**")
         submitted = st.form_submit_button()
 
         ###
@@ -35,8 +40,8 @@ def add_values():
             new_entry = {'Date':new_date, 'PS_Q_DAY':new_ps_q_day, 'TPS_Q1_DAY':new_tps_q1_day, 'TWAS_DAF_QIN_DAY':new_twas_daf_qin_day, \
                         'DIGESTED_SLUDGE_QOUT_DAY':new_dig_s_qout_day, 'BIOGAS_PRODUCTION_Q_DAY':0, \
                             'DIG_SLUDGE_DEWATER_DS_AFTER_DEWATER_3_PER_WEEK':new_dig_s_dwtr_ds_after_per_week}
-            new_entry = pd.Series(new_entry)
-            st.session_state.data = pd.concat([st.session_state.data, new_entry.to_frame().T]).reset_index(drop=True)
+            append_data(new_entry)
+            st.success("Data added successfully!")
 
 def add_metrics():
 
@@ -75,8 +80,8 @@ if __name__ == "__main__":
         layout="wide",
     )
     df = get_data()
+
     # Title
-    # st.title("Biogas production")
     st.markdown("<h1 style='text-align: center; color: black;'>Biogas production</h1>", unsafe_allow_html=True)
 
     placeholder = st.empty()
